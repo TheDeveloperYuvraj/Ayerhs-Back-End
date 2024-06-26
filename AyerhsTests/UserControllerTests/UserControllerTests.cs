16a7742ced877463cdccdcfcd48445e41e4681d2
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-06-26 13:55:42 +0530

    Write unit test cases for `GetPartitions` endpoint

--- AyerhsTests/UserControllerTests/UserControllerTests.cs
+++ AyerhsTests/UserControllerTests/UserControllerTests.cs
@@ -1,9 +1,11 @@
 ﻿using Ayerhs.Controllers;
+using Ayerhs.Core.Entities.UserManagement;
 using Ayerhs.Core.Entities.Utility;
 using Ayerhs.Core.Interfaces.UserManagement;
 using Bogus;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 using Moq;
 
 namespace AyerhsTests.UserControllerTests
@@ -73,6 +75,66 @@ namespace AyerhsTests.UserControllerTests
 
             // Act
             var result = await _controller.AddPartition(partitionName);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var response = Assert.IsType<ApiResponse<string>>(badRequestResult.Value);
+            Assert.Equal("Error", response.Status);
+            Assert.Equal(500, response.StatusCode);
+            Assert.Equal(0, response.Response);
+            Assert.Equal(exceptionMessage, response.ReturnValue);
+        }
+
+        [Fact]
+        public async Task GetPartitions_Success()
+        {
+            // Arrange
+            var partitions = new List<Partition>
+        {
+            new Partition { PartitionId = _faker.Random.Guid().ToString(), PartitionName = _faker.Lorem.Word(), PartitionCreatedOn = DateTime.UtcNow, PartitionUpdatedOn = DateTime.UtcNow },
+            new Partition { PartitionId = _faker.Random.Guid().ToString(), PartitionName = _faker.Lorem.Word(), PartitionCreatedOn = DateTime.UtcNow, PartitionUpdatedOn = DateTime.UtcNow }
+        };
+            _mockUserService.Setup(s => s.GetPartitionsAsync()).ReturnsAsync(partitions);
+
+            // Act
+            var result = await _controller.GetPartitions();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<ApiResponse<List<Partition>>>(okResult.Value);
+            Assert.Equal("Success", response.Status);
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(1, response.Response);
+            Assert.Equal(partitions, response.ReturnValue);
+        }
+
+        [Fact]
+        public async Task GetPartitions_EmptyList()
+        {
+            // Arrange
+            _mockUserService.Setup(s => s.GetPartitionsAsync()).ReturnsAsync((List<Partition>)null!);
+
+            // Act
+            var result = await _controller.GetPartitions();
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var response = Assert.IsType<ApiResponse<string>>(badRequestResult.Value);
+            Assert.Equal("Error", response.Status);
+            Assert.Equal(400, response.StatusCode);
+            Assert.Equal(0, response.Response);
+            Assert.Equal(null!, response.ReturnValue);
+        }
+
+        [Fact]
+        public async Task GetPartitions_UnknownError()
+        {
+            // Arrange
+            var exceptionMessage = "An unexpected error occurred.";
+            _mockUserService.Setup(s => s.GetPartitionsAsync()).ThrowsAsync(new Exception(exceptionMessage));
+
+            // Act
+            var result = await _controller.GetPartitions();
 
             // Assert
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
